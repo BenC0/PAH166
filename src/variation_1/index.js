891cda5b8492abf58fdd6f531554660035fb3a82
BenC0 <suddensquirrel94@gmail.com>
2022-06-16 09:25:19 +0100

    init commit

---
+++ src/variation_1/index.js
@@ -0,0 +1,21 @@
+import { Variant, TestElement, TestElements } from "../../../dist/index"
+
+const test_config = {
+    id: "ex002",
+    google_analytics: 29,
+    hotjar: false
+}
+const conditions = _ => {
+    return !!document.querySelector("body")
+}
+
+function action() {
+    this.log("Action loaded")
+}
+
+function fallback() {
+    this.log("Test can't run, fallback loaded", true)
+}
+
+const variation = new Variant(test_config, "Variation 1", conditions, action, fallback)
+variation.run()
