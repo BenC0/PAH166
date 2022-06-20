d242410043b4ca052ed39fe8d4d4d848d27adad8
BenC0 <suddensquirrel94@gmail.com>
2022-06-20 10:38:45 +0100

    Added checkboxes, upgraded the original facet selection to use formatted values instead of integer key references. This should allow for more robust selection, but additional work still required for after FacetOption is selected

--- src/plp/filter/index.js
+++ src/plp/filter/index.js
@@ -1,15 +1,26 @@
 import "./index.css"
 import { TestElements, TestElement, getHighestZIndex } from "../../norman";
 
+export function formatFacetName(name) { 
+    name = name.trim()
+    name = name.replace(/  /g, " ");
+    name = name.replace(/ /g, "_");
+    name = name.replace(/&/g, "and");
+    name = name.replace(/"|\<|\>|&/g, "");
+    return name
+}
+
 export class FacetOption {
     constructor(option) {
-        this.original_node = option
+        this.original_node = option.node
         this.quantity = ""
         if (!!option.node.querySelector('.side-nav-level2__quantity')) {
             this.quantity = option.node.querySelector('.side-nav-level2__quantity').textContent
         }
         this.is_active = option.node.querySelector('.side-nav-level2__box').classList.contains('active')
         this.name = option.node.getAttribute("title")
+        this.formatted_name = formatFacetName(this.name)
+        this.original_node.setAttribute("option", this.formatted_name)
     }
 }
 
@@ -17,8 +28,10 @@ export class Facet {
     constructor(facet) {
         this.header = facet._find(".side-nav-level0__header").pop()
         this.header_name = this.header._text()
+        this.formatted_name = formatFacetName(this.header_name)
         this.values = []
         this.active_values = []
+        facet.node.setAttribute("facet", this.formatted_name)
         facet._find(`.side-nav-level2__header`).forEach(element => {
             let fO = new FacetOption(element)
             this.values.push(fO)
@@ -57,13 +70,14 @@ export class FilterButton extends TestElement {
 }
 
 export class TestFilter {
-    constructor(facets) {
-        this.facets = facets.facets
+    constructor(facets, state) {
+        this.facets = facets
+        this.state = state
         this.facets_html = []
-        this.facets.forEach((facet, index) => {
+        this.facets.facets.forEach((facet, index) => {
             this.facets_html.push(this._create_facet_html(facet, index))
         })
-        this.html = `<div test="pah166" class="filters" style="z-index: ${getHighestZIndex("*") + 1}">
+        this.html = `<div test="pah166" class="filters ${this.state}" style="z-index: ${getHighestZIndex("*") + 1}">
             <div class="background"></div>
             <div class="facets">
                 <header>
@@ -84,30 +98,32 @@ export class TestFilter {
         this.facet_options = this.element._find(".facet_option")
         this.facet_options.forEach(option => {
             option.node.addEventListener("click", e => {
-                this._select_facet_option(e.currentTarget, this.facets)
+                this._select_facet_option(e.currentTarget, this.facets.facets)
+                this._refresh_factes()
             })
         })
     }
 
     _refresh_factes() {
         // TODO: Update options on click, this needs to be dynamic with as little perceived flicker as possible...
-        this.facets = new Facets().facets
+        this.facets = new Facets()
         console.warn(this.facets)
     }
 
     _create_option_html(option, index) {
-        return `<li class="facet_option" option-index="${index}">
+        return `<li class="facet_option" option="${option.formatted_name}" is_active="${option.is_active}">
+            <span class="checkbox"></span>
             <span class="name">${option.name}</span>
             <span class="quantity">${option.quantity}</span>
         </li>`
     }
     
     _create_facet_html(facet, index) {
-        let header_html = `<header class="facet_header">
+        let header_html = `<header class="facet_header" facet="${facet.formatted_name}">
             <span class="name">${facet.header_name}</span>
             <span class="chevron">></span>
         </header>`
-        let options_html = `<ul class="facet_options" facet-index="${index}">
+        let options_html = `<ul class="facet_options" facet="${facet.formatted_name}">
             ${facet.values.map((value, index) => this._create_option_html(value, index)).join("")}
         </ul>`
         return `<div class="facet">
@@ -124,12 +140,18 @@ export class TestFilter {
         }
     }
 
+    _find_facet_option(target) {
+        let facet_index = target.parentNode.getAttribute("facet");
+        let target_facet = document.querySelector(`.side-nav-level0[facet="${facet_index}"]`)
+        let option_index = target.getAttribute("option");
+        let target_option = target_facet.querySelector(`[option="${option_index}"]`)
+        console.warn(facet_index, option_index)
+        return target_option
+    }
+
     _select_facet_option(target, facets) {
-        let facet_index = parseInt(target.parentNode.getAttribute("facet-index"));
-        let option_index = parseInt(target.getAttribute("option-index"));
-        let selected_option = facets[facet_index].values[option_index]
-        selected_option.original_node.node.click()
-        this._refresh_factes()
+        let selected_option = this._find_facet_option(target)
+        selected_option.click()
     }
 }
 
@@ -139,7 +161,7 @@ export class Filter extends TestElement {
         this.facets = new Facets()
         this.cta = new FilterButton()
         this.cta._insert(".sort_and_filter_container", "afterBegin")
-        this.new_filters = new TestFilter(this.facets)
+        this.new_filters = new TestFilter(this.facets, "")
         this.cta.node.addEventListener("click", e => {
             this._toggle_filters()
         })
